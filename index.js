c785559b86f18ba62c39f1221e915b928f2de352
kimon-satan <simon@simonkatan.co.uk>
2018-01-22 15:33:31 +0000

    added igor email

--- index.js
+++ index.js
@@ -65,13 +65,9 @@ nodemailer.createTestAccount((err, account) => {
 	{
 		// init reusable transporter object using the default SMTP transport
 		app.transporter = nodemailer.createTransport({
-				host: 'smtp.ethereal.email',
-				port: 587,
-				secure: false, // true for 465, false for other ports
-				auth: {
-						user: account.user, // generated ethereal user
-						pass: account.pass  // generated ethereal password
-				}
+				host: 'igor.gold.ac.uk', //we should now be able to email igor accounts
+				port: 25,
+				secure: false // true for 465, false for other ports
 		});
 	}
 })
